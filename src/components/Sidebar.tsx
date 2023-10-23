96710764f96f5d177293d564aba97bb98d928333
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2023-10-23 16:06:31 +0200

    added MenuItemProps

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,7 +11,12 @@ export function Sidebar() {
   );
 }
 
-function MenuItem({ to, label, children }) {
+interface MenuItemProps {
+  to: string;
+  label: string;
+}
+
+function MenuItem({ to, label }: MenuItemProps) {
   return (
     <NavLink className="menu-item" to={to}>
       {label}
